855e882b0978cfe29a0152114b49715b55a4a5c5
Neil Horman <nhorman@tuxdriver.com>
2018-05-10 09:28:41 -0400

    rng-tools: Add some test scripts
    Add in some make check targets to enable validation of code with travis
    ci
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

---
+++ tests/rngtesturandom.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Confirm that /dev/urandom produces enough randomness to pass the FIPS-140-2
+# randomness validation tests.  This isn't a great test, but it should work, and
+# failure indicates some problems in /dev/urandom (or the test) anyway
+cat /dev/urandom | rngtest -c 100 --pipe >/dev/null
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
+
+exit 0
---
+++ tests/rngtestzero.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Test that we catch a repeating zero failure in rngtest
+cat /dev/zero | rngtest -c 100 --pipe > /dev/null
+if [ $? -eq 0 ]
+then
+	exit 1
+fi
+
+exit 0
+
