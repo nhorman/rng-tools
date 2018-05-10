da5c5cd94439a63d0a6be8538d22d73f6752d3ef
Neil Horman <nhorman@tuxdriver.com>
2018-05-10 10:34:05 -0400

    Fix up travis ci config for self test scripts

--- tests/rngtestzero.sh
+++ tests/rngtestzero.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Test that we catch a repeating zero failure in rngtest
-cat /dev/zero | rngtest -c 100 --pipe > /dev/null
+cat /dev/zero | ../rngtest -c 100 --pipe > /dev/null
 if [ $? -eq 0 ]
 then
-	cat tests/test-suite.log
 	exit 1
 fi
 
