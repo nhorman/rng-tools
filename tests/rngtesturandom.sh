5cb91bd9053fe17305f2c8bd29a7c6a8c9b29177
Neil Horman <nhorman@tuxdriver.com>
2018-05-10 10:28:40 -0400

    rngtesturandom: skip test if urandom doesn't exist
    Can't run the test without the device
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- tests/rngtesturandom.sh
+++ tests/rngtesturandom.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -r /dev/urandom ]
+then
+	echo "NO URANDOM DEVICE, SKIPPING TEST"
+	exit 77
+fi
+
 # Confirm that /dev/urandom produces enough randomness to pass the FIPS-140-2
 # randomness validation tests.  This isn't a great test, but it should work, and
 # failure indicates some problems in /dev/urandom (or the test) anyway
