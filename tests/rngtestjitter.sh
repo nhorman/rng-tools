989d5ba320b07303ccb555d95057ab2ab95c1241
Neil Horman <nhorman@tuxdriver.com>
2018-09-13 10:34:24 -0400

    tests: Add tests for jitter entropy source
    Since the JITTER source is cpu and hardware agnostic, run some tests
    using it
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

---
+++ tests/rngtestjitter.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Test that we catch a repeating zero failure in rngtest
+./rngd -f -x hwrng -x rdrand -x tpm -O jitter:use_aes:1 | ../rngtest -c 100 --pipe > /dev/null
+if [ $? -eq 0 ]
+then
+	exit 1
+fi
+
+exit 0
+
