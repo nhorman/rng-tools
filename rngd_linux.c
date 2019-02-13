a88c630acb6e1c0403a74990b8f4e63468819fe7
Neil Horman <nhorman@tuxdriver.com>
2019-02-13 08:58:35 -0500

    Use RNDGETENTCNT to gate sleeping in rngd
    Recently introduced changes to create hysteresis in waking and sleeping
    of rngd were not completed.  Specifically testing for the size of the
    entropy pool was broken as it was assumed the RNDADDENTCNT ioctl
    returned the resultant fill size of the entropy pool.  We actually need
    to use RNDGETENTCNT to retrieve that number, allowing rngd to go back to
    sleep when the pool is full
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- rngd_linux.c
+++ rngd_linux.c
@@ -139,11 +139,18 @@ int random_add_entropy(void *buf, size_t size)
 					strerror(errno));
 				return -1;
 			}
+
+			if (ioctl(random_fd, RNDGETENTCNT, &ent->ent_count) != 0)
+				message(LOG_DAEMON|LOG_ERR, "Failed to get Entropy count!\n");
+			ent->ent_count *= 8; /*adjust for the kernel shift*/
+		} else {
+			if (ioctl(random_fd, RNDGETENTCNT, &ent->ent_count) != 0)
+				message(LOG_DAEMON|LOG_ERR, "Failed to get Entropy count!\n");
 		}
 	} else
 		write(random_fd, buf, size);
 
-	return ent->ent_count;
+	return ent->ent_count; 
 
 }
 
