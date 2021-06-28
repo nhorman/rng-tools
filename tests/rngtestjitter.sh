aee707089d3185fb89740ca305b55f8b63ac7d6d
Vladis Dronov <vdronov@redhat.com>
2021-06-28 17:51:11 +0200

    A small output and a test fix
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>

--- tests/rngtestjitter.sh
+++ tests/rngtestjitter.sh
@@ -2,7 +2,7 @@
 
 # Test that we catch a repeating zero failure in rngtest
 ./rngd -f -o /dev/stdout -x hwrng -x rdrand -x tpm -O jitter:use_aes:1 | ../rngtest -c 100 --pipe > /dev/null
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
 	exit 1
 fi
