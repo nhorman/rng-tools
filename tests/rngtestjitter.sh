f826979ff9351f3a797274d2a1ffc1d86b46c7f8
Neil Horman <nhorman@gmail.com>
2021-08-20 13:12:27 -0400

    Fixing rngtestjitter.sh
    Wasn't running properly, needed to fix the rngd path and kill the
    process after a fixed time.
    
    resolves https://github.com/nhorman/rng-tools/issues/147
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- tests/rngtestjitter.sh
+++ tests/rngtestjitter.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Test that we catch a repeating zero failure in rngtest
-./rngd -f -o /dev/stdout -x hwrng -x rdrand -x tpm -O jitter:use_aes:1 | ../rngtest -c 100 --pipe > /dev/null
-if [ $? -ne 0 ]
-then
-	exit 1
-fi
+kill_rngd() {
+	sleep 5
+	echo "killing"
+	killall -9 rngd
+}
 
-exit 0
+kill_rngd &
+
+../rngd -f -o /dev/stdout -x hwrng -x rdrand -x tpm -O jitter:use_aes:1 | ../rngtest -c 100 --pipe > /dev/null
+
 
